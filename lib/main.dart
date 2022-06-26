e695c03ede4b4eae5f3954019ca7c485609353af
svaykong <kongsvay55@gmail.com>
2022-06-26 12:12:28 +0700

    adding flutter_dotenv library

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-// import 'dart:developer';
-// import 'dart:io' show Platform;
-
-// import 'dart:developer';
+import 'dart:developer' as dev_tool show log;
 
 import 'package:flutter/material.dart';
 
 import 'package:day_night_switcher/day_night_switcher.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'notifiers/theme_notifier.dart';
-// import 'utils/color_util.dart';
 
 void main() {
   // should call this to prevent error in some case
@@ -21,6 +18,12 @@ void main() {
       child: const MyApp(),
     ),
   );
+}
+
+extension Log on Object {
+  void log(message) {
+    dev_tool.log(message.toString());
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -51,13 +54,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool _isDarkModeEnabled = false;
 
+  Future<Map<String, String>> _loadEnv() async {
+    await Future.delayed(const Duration(seconds: 1), () async {
+      // load dotenv
+      await dotenv.load();
+    });
+
+    return dotenv.env;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _isDarkModeEnabled = context.read<ThemeNotifier>().getDarkModePres();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Platform.isMacOS
-
-    const profileImageUrl =
-        '';
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -65,11 +79,47 @@ class _MyHomePageState extends State<MyHomePage> {
       drawer: Drawer(
         child: Column(children: [
           DrawerHeader(
-            child: CircleAvatar(
-              radius: 80,
-              child: ClipOval(
-                child: Image.network(profileImageUrl),
-              ),
+            child: FutureBuilder<Map<String, String>>(
+              future: _loadEnv(),
+              builder: (context, AsyncSnapshot<Map<String, String>?> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                    return const Center(child: CircularProgressIndicator());
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text(
+                          "Error::${snapshot.error}",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      );
+                    } else if (snapshot.hasData == false) {
+                      return Center(
+                        child: Text(
+                          "No data",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      );
+                    }
+                    final profileURL = snapshot.data!["PROFILE_URL"];
+                    if (profileURL == null) {
+                      return Center(
+                        child: Text(
+                          "No data",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      );
+                    }
+                    return CircleAvatar(
+                      radius: 80,
+                      child: ClipOval(
+                        child: Image.network(profileURL),
+                      ),
+                    );
+                }
+              },
             ),
           ),
           DayNightSwitcher(
